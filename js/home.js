42e277e8f1bbaddc27202b41f29c9def9da715a7
Swaraj Gandhi <gandhiswaraj9067008148@gmail.com>
2015-04-21 22:46:14 +0530

    add content

--- js/home.js
+++ js/home.js
@@ -8,12 +8,14 @@ function init() {
       		$(".navbar__list").addClass("smaller");
       		$(".navbar__link").addClass("smaller");
       		$(".navbar__submenu").addClass("smaller");
+          $(".navbar__logo").addClass("smaller");
       } else {
           if (classie.has(header,"smaller")) {
       		$(header).removeClass("smaller");
           		$(".navbar__list").removeClass("smaller");
-  				$(".navbar__link").removeClass("smaller");
-  				$(".navbar__submenu").removeClass("smaller");
+  			   	  $(".navbar__link").removeClass("smaller");
+  				    $(".navbar__submenu").removeClass("smaller");
+              $(".navbar__logo").removeClass("smaller");
           }
       }
   });
@@ -51,6 +53,42 @@ $(document).ready(function(){
     var w = $(window).width();
     if(w > 320 && menu.is(':hidden')) {
         menu.removeAttr('style');
+    }
+  });
+
+  $("#check").click(function(){
+    var height = parseInt($("#height").val());
+    var weight = parseInt($("#weight").val());
+    var age = parseInt($("#age").val());
+    if($.isNumeric(height)) {
+
+    }
+    else {
+      alert('Please Enter valid Height.Only Integer number allowed');
+    }
+    if($.isNumeric(weight)) {
+      
+    }
+    else {
+      alert('Please Enter valid Weight.Only Integer number is allowed');
+    }
+
+    if($.isNumeric(age)) {
+      
+    }
+    else {
+      alert('Please Enter valid age.Only Integer number is allowed');
+    }
+
+    var answer;
+    if ($("#male").prop("checked")) {
+      answer = 66 + ( 13.7 * weight) + ( 5 * height) - ( 6.8 * age);
+      alert("Your BMI is Man"+answer);
+    }else {
+      if($("#female").prop("checked")){
+        answer =  655 + ( 9.6 * weight) + ( 1.8 * height) - ( 4.7 * age);
+        alert("Your BMI is  woman"+answer);
+      }
     }
   });
 
